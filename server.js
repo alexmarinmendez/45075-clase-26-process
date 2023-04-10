0b25272d4986bde70f1775b7796c5c60fd4e68c9
Alex Marin Mendez <alexmarinmendez@gmail.com>
2023-04-10 18:48:33 -0500

    feat: express server with commander

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 const express = require('express')
+const { Command } = require('commander')
 
 const app = express()
+const program = new Command()
 
-const port = process.argv.slice(2)[0]
+program
+    .option('-p <port>', 'Puerto del servidor', 8080)
+
+program.parse()
+
+const port = program.opts().p
 
 app.listen(port, () => console.log(`Server Up on port ${port}`))
